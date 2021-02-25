f657ac2484ba0bd2b5fe4d3523178a20cc48a24c
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-02-25 02:24:24 -0300

    initil

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TinaProvider, TinaCMS, useCMS, useForm, usePlugin } from 'tinacms';
 
 function App() {
+  const [pageData, setPageData] = React.useState({
+    title: "Teste",
+    body: "Teste de conteúdo tinaCMS"
+  })
+
+  function PageContent() {
+
+    // 2. Define the form configuration object
+    const formConfig = {
+      id: 'tina-tutorial-index',
+      label: 'Edit Page',
+      fields: [
+        {
+          name: 'title',
+          label: 'Title',
+          component: 'text',
+        },
+        {
+          name: 'body',
+          label: 'Body',
+          component: 'textarea',
+        },
+      ],
+      initialValues: pageData,
+      onSubmit: async (content) => {
+        setPageData(content);
+      },
+    }
+
+    // 3. Create the form
+    const [editableData, form] = useForm(formConfig)
+
+    // 4. Register it with the CMS
+    usePlugin(form)
+
+    return (
+      <section className="App-header">
+        {/**
+           * 5. Render the `editableData` returned from `useForm`
+           */}
+        <h1>{editableData.title}</h1>
+        <p>{editableData.body}</p>
+        <EditButton />
+      </section>
+    );
+  }
+
+  const cms = new TinaCMS({
+    sidebar: true,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TinaProvider cms={cms}>
+      <div className="App">
+        <p>Tina CMS</p>
+      </div>
+      <PageContent />
+    </TinaProvider>
+  );
+}
+
+function EditButton() {
+  const cms = useCMS();
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+    </button>
   );
 }
 
